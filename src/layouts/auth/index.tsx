91a7508ad658a1f22e6b271e9915685f72c5c92c
nguyencongson96 <chansen2912@gmail.com>
2023-08-11 21:02:41 +0700

    update auth

--- src/layouts/auth/index.tsx
+++ src/layouts/auth/index.tsx
@@ -1,14 +1,8 @@
 import React, { ReactElement } from "react";
-import styles from "@/styles/Auth.module.scss";
+import styles from "@/styles/auth/Auth.module.scss";
 
-const AuthLayout = ({ children }: {children: ReactElement}) => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.circle} id={styles.circle_1}></div>
-      <div className={styles.circle} id={styles.circle_2}></div>
-      {children}
-    </div>
-  );
+const AuthLayout = ({ children }: { children: ReactElement }) => {
+  return <div className={styles.container}>{children}</div>;
 };
 
 export default AuthLayout;
